4eeeb88f7b21b2951e9fe56f3229277e9d10bf4b
karpet <karpet@3ef08107-bd00-0410-9448-96cd035bd1f9>
2006-07-05 20:18:42 +0000

    added examples to dist
    git-svn-id: http://svn.peknet.com/perl/trunk/Search-Tools@173 3ef08107-bd00-0410-9448-96cd035bd1f9

--- example/dirsearch.pl
+++ example/dirsearch.pl
@@ -6,7 +6,6 @@
 use strict;
 use warnings;
 
-no Time::TimeTick;
 use File::Slurp;
 use Search::Tools;
 
@@ -15,7 +14,6 @@ my $usage = "$0 'query' file(s) \n";
 my $query = shift @ARGV or die $usage;
 my @files = @ARGV       or die $usage;
 
-timetick('start snipper');
 my $snipper =
   Search::Tools->snipper(
                          query               => $query,
@@ -24,34 +22,16 @@ my $snipper =
                          occur               => 4,
                          context             => 12
                         );
-timetick('  end snipper');
 
-timetick('start hiliter w/ compiled query and tty');
 my $hiliter = Search::Tools->hiliter(query => $snipper->rekw, tty => 1);
-timetick('  end hiliter w/ compiled query and tty');
 
 for my $f (@files)
 {
-
-    # print "FILE: $f\n";
-
-    timetick('start file');
     my $text = read_file($f);
-    timetick('  end file');
-
-    timetick('start snip');
     my $snip = $snipper->snip($text);
-    timetick('  end snip');
-
     if (!$snip)
     {
-
-        #warn "no snip for $f\n";
         next;
     }
-
-    timetick('start light');
     print "$f: " . $hiliter->light($snip), $/;
-    timetick('  end light');
-
 }
