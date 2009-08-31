6c60fd58034155ce6d7d7477d60323dd4ee64582
karpet <karpet@3ef08107-bd00-0410-9448-96cd035bd1f9>
2009-08-31 16:49:05 +0000

    forgot a file
    git-svn-id: http://svn.peknet.com/perl/branches/Search-Tools/xs-tokenizer@2033 3ef08107-bd00-0410-9448-96cd035bd1f9

---
+++ lib/Search/Tools/TokenList.pm
@@ -0,0 +1,13 @@
+package Search::Tools::TokenList;
+use strict;
+use warnings;
+use overload
+    '""'     => sub { $_[0]->str; },
+    'bool'   => sub { $_[0]->len; },
+    fallback => 1;
+
+use Search::Tools;    # XS required
+
+our $VERSION = '0.24';
+
+1;
