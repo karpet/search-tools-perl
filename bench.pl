069dce6e645f66fec2c0e58f895397c677f2e444
karpet <karpet@3ef08107-bd00-0410-9448-96cd035bd1f9>
2009-09-01 12:47:15 +0000

    refactor the benchmark script for a little closer approx in pp to what xs is doing. amazing how moving the to_utf8() check increases ascii performance:
                       Rate pure-perl-greek     xs-greek pure-perl-ascii    xs-ascii
    pure-perl-greek  2552/s              --         -44%            -87%        -90%
    xs-greek         4587/s             80%           --            -76%        -81%
    pure-perl-ascii 19493/s            664%         325%              --        -20%
    xs-ascii        24331/s            854%         430%             25%          --
    
    
    
    
    git-svn-id: http://svn.peknet.com/perl/branches/Search-Tools/xs-tokenizer@2048 3ef08107-bd00-0410-9448-96cd035bd1f9

--- bench.pl
+++ bench.pl
@@ -6,8 +6,8 @@ use Search::Tools::UTF8;
 use Benchmark qw(:all);
 use File::Slurp;
 
-my $greek = read_file('t/docs/greek_and_ojibwe.txt');
-my $ascii = read_file('t/docs/ascii.txt');
+my $greek = to_utf8( read_file('t/docs/greek_and_ojibwe.txt') );
+my $ascii = to_utf8( read_file('t/docs/ascii.txt') );
 my $regex = qr/\w+(?:'\w+)*/;
 
 cmpthese(
@@ -30,17 +30,17 @@ sub heat_seeker {
 }
 
 sub pure_perl {
-    my @tokens = split( m/($regex)/, to_utf8( $_[0] ) );
-    my %markers;
-    my $i = 0;
-    for (@tokens) {
-        $markers{$i} = {
-            'pos'    => $i,
-            str      => $_,
-            is_hot   => heat_seeker($_),
-            is_match => ( $_ =~ m/^$regex$/ ) ? 1 : 0,
-        };
-        $i++;
+    my @tokens = ();
+    my $i      = 0;
+    for ( split( m/($regex)/, $_[0] ) ) {
+        push(
+            @tokens,
+            {   'pos'    => $i++,
+                str      => $_,
+                is_hot   => heat_seeker($_),
+                is_match => ( $_ =~ m/^$regex$/ ) ? 1 : 0,
+            }
+        );
     }
 
 }
