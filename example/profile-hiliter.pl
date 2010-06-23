f21539e5a81f2d0f82f3e951f98866b964484338
karpet <karpet@3ef08107-bd00-0410-9448-96cd035bd1f9>
2010-06-23 00:55:09 +0000

    add benchmarker
    git-svn-id: http://svn.peknet.com/perl/Search-Tools/trunk@2824 3ef08107-bd00-0410-9448-96cd035bd1f9

--- example/profile-hiliter.pl
+++ example/profile-hiliter.pl
@@ -5,14 +5,15 @@ use Search::Tools;
 use Search::Tools::HiLiter;
 use Search::Tools::XML;
 use File::Slurp;
-use Path::Class;
+use Benchmark qw(:all);
 
 my $buf     = read_file('t/docs/ascii.txt');
 my $query   = Search::Tools->parser->parse('thronger');
 my $hiliter = Search::Tools::HiLiter->new( query => $query, );
-my $count   = 0;
-while ( $count++ < 10000 ) {
-
-    my $lit = $hiliter->light($buf);
-
-}
+cmpthese(
+    1000,
+    {   'hilite' => sub {
+            my $lit = $hiliter->light($buf);
+        },
+    }
+);
